9571da5139a08d72b44d8a72c606f62a5eeccbd8
adrian <fx.adrian@gmail.com>
2025-08-23 17:15:51 -0400

    add routes and navlinks

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,14 +1,30 @@
-import {NavLink} from "react-router-dom";
-
+import { NavLink } from "react-router";
 import styles from "./NavigationBar.module.css";
 
-function NavigationBar ()
-{
-	return (
-		{
-			// todo
-		}
-	);
+const appLinks = [
+  { path: "/", content: "🏨 Home" },
+  { path: "/spacecraft", content: "🚀 Spacecraft" },
+  { path: "/planets", content: "🌎 Planets" },
+];
+
+function NavigationBar() {
+  function setActive({ isActive }) {
+    return isActive ? styles["navbar__item-active"] : "";
+  }
+
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navbar__list}>
+        {appLinks.map(({ path, content }, i) => (
+          <li key={i} className={styles.navbar__item}>
+            <NavLink className={setActive} to={path}>
+              {content}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default NavigationBar;
--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,17 +1,23 @@
-import {Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
 
-import Home from "../pages/Home/Home";
-import Spacecrafts from "../pages/Spacecrafts/Spacecrafts.jsx";
-import SpacecraftBuild from "../pages/SpacecraftBuild/SpacecraftBuild.jsx";
-import Spacecraft from "../pages/Spacecraft/Spacecraft.jsx";
-import Planets from "../pages/Planets/Planets.jsx";
+// import Home from "../pages/Home/Home";
+// import Spacecrafts from "../pages/Spacecrafts/Spacecrafts.jsx";
+// import SpacecraftBuild from "../pages/SpacecraftBuild/SpacecraftBuild.jsx";
+// import Spacecraft from "../pages/Spacecraft/Spacecraft.jsx";
+// import Planets from "../pages/Planets/Planets.jsx";
 
-function AppRoute ()
-{
+function AppRoute() {
   return (
-    {
-      // todo render routes
-    }
+    <Routes>
+      <Route path={"/"} element={<div>home</div>} />
+      <Route path={"/spacecraft"}>
+        <Route index element={<div>spacecraft</div>} />
+        <Route path={":id"} element={<div>spacecraft-ITEM</div>} />
+        <Route path={"build"} element={<div>spacecraft-BUILD</div>} />
+      </Route>
+      <Route path={"/planets"} element={<div>planets</div>} />
+      <Route path="*" element={<div>Not found</div>} />
+    </Routes>
   );
 }
 
